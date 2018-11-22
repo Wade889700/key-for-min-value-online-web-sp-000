bffc63a01c2e20e555bafbd8390a0be864adac0b
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-22 09:05:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   name_hash.collect do |name, value|
     if (value[0] > value[1] >value[2]) || (value[1] > value[0] > value[2])
       return name[2]
-    elsif (value[0] > value[2] > value[1]) || (value[2] > value[0] > value[1])
+    elsif (value[0] > value[2] && value[2] > value[1]) || (value[2] > value[0] && value[0] > value[1])
       return name[1]
     else
         return name[0]
