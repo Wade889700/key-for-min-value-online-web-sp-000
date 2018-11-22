c45f8682155e49de75c6800870dfc25d30b94fec
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-22 09:08:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash ={}
+    return nil
+  end
   name_hash.collect do |name, value|
     if (value[0] > value[1] && value[1]>value[2]) || (value[1] > value[0] && value[0] > value[2])
       return name[2]
@@ -9,8 +12,7 @@ def key_for_min_value(name_hash)
       return name[1]
     elsif (value[1] > value[2] && value[2] > value[0]) || (value[2] > value[1] && value[1] > value[0])
         return name[0]
-    else
-        return nil
+      
     end
   end
 end
