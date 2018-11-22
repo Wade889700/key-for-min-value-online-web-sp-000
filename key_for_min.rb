b59614475084e4bb50017d65c6e8e75397eeebe6
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-22 09:26:12 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_value = 0
   lowest_key = nil
   name_hash.each do |name, value|
-    if lowest==nil || value<lowest_value
+    if lowest_value == 0 || value<lowest_value
       lowest_value = value
       lowest_key = name
     end
